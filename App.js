725df0e23a66db01d9ced832aada7b1a3787f1aa
quyenluu <quyenluu1989@gmail.com>
2018-05-28 23:05:15 +0700

    init source

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import { Provider } from 'react-redux';
+//import {  } from '';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          Welcome to React Native! gggg hhh
+        </Text>
+      </View>
+    );
+  }
+}
+
