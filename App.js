90104b7a423fdcd2c2cc5bc17fafc033a84ec106
quyen <quyenluu1989@gmail.com>
2018-05-29 17:59:43 +0700

    update code

--- App.js
+++ App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import { Provider } from 'react-redux';
-//import {  } from '';
+import {  } from '';
 
 export default class App extends Component {
   render() {
     return (
       <View>
         <Text>
-          Welcome to React Native! gggg hhh
+          Welcome to React Native! gggg hhh ádas sdfsdf
         </Text>
       </View>
     );
---
+++ src/navigations/AppNavigator.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+import { createStackNavigator } from 'react-navigation';
+
+export const AppNavigator = createStackNavigator(
+
+)
