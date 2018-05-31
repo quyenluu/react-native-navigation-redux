e272726ad34942fe40ba53b67c90e8247f9ba3ee
quyenluu <quyenluu1989@gmail.com>
2018-05-31 23:28:01 +0700

    update code navigation

--- src/components/Auth/LoginScreen.js
+++ src/components/Auth/LoginScreen.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 export default class LoginScreen extends Component {
     static navigationOptions = {
         title: 'Login Title',
@@ -9,6 +11,9 @@ export default class LoginScreen extends Component {
         return (
             <View style={{ flex: 1, backgroundColor: 'red', alignItems: 'center', justifyContent: 'center' }}>
                 <Text>Login Screen</Text>
+                <Ionicons name='ios-options' size={25} color='blue'>
+                    <Text>Hello</Text>
+                </Ionicons>
             </View>
         );
     }
--- src/navigations/AppNavigator.js
+++ src/navigations/AppNavigator.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 
-import { createStackNavigator } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation';
 import { AuthStack } from '../components/Auth/AuthStack';
 import { AppStack } from '../components/App/AppStack';
 
-export const AppNavigator = createStackNavigator(
+export const AppNavigator = createBottomTabNavigator(
     {
         //AuthLoading: AuthLoadingScreen,
         App: AppStack,
         Auth: AuthStack,
     },
     {
-        initialRouteName: 'Auth'
+        initialRouteName: 'Auth',
+        navigationOptions: ({ navigation }) => ({
+            tabBarIcon: ({ focused, tintColor }) => {
+                const { routeName } = navigation.state;
+                let iconName;
+                if (routeName === 'Home') {
+                    iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+                } else if (routeName === 'Settings') {
+                    iconName = `ios-options${focused ? '' : '-outline'}`;
+                }
+        
+                return <Ionicons name={iconName} size={25} color={tintColor} />;
+            },
+        }),
+        tabBarOptions: {
+            activeTintColor: 'tomato',
+            inactiveTintColor: 'gray',
+        }
     }
 )
